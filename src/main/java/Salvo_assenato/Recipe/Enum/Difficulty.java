dceadefef9759e6e8d3d03384de121214f4c3d9b
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-10-06 17:31:54 +0200

    aggiunti altri Enum

---
+++ src/main/java/Salvo_assenato/Recipe/Enum/Difficulty.java
@@ -0,0 +1,8 @@
+package Salvo_assenato.Recipe.Enum;
+
+public enum Difficulty {
+
+    EASY,   // FACILE
+    MEDIUM, // MEDIA
+    HARD,   // DIFFICILE
+}
---
+++ src/main/java/Salvo_assenato/Recipe/Enum/DishCategory.java
@@ -0,0 +1,12 @@
+package Salvo_assenato.Recipe.Enum;
+
+public enum DishCategory {
+    APPETIZER,   // Antipasto
+    MAIN_COURSE, // Piatto principale
+    DESSERT,     // Dessert
+    SIDE_DISH,   // Contorno
+    SALAD,       // Insalata
+    SOUP,        // Zuppa
+    SNACK,       // Spuntino
+    BEVERAGE;    // Bevanda
+}
