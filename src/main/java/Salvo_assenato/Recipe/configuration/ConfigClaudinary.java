e784b444cb44d86e6817c479a792755cdb59f4b1
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-10-18 11:18:41 +0200

    Update ConfigClaudinary.java
    sistemazione la classe configurazione di cloudinary

--- src/main/java/Salvo_assenato/Recipe/configuration/ConfigClaudinary.java
+++ src/main/java/Salvo_assenato/Recipe/configuration/ConfigClaudinary.java
@@ -3,10 +3,11 @@ package Salvo_assenato.Recipe.configuration;
 import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@Configuration
 public class ConfigClaudinary {
 
     @Bean
